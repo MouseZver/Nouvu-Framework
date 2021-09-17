99f57566b4362d413c408b237c7c2ee151edffa5
Mouse <mouse-zver@xaker.ru>
2021-09-18 00:50:45 +0300

    Create BackController.php

---
+++ Nouvu/Http/Controllers/BackController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare ( strict_types = 1 );
+
+namespace Nouvu\Web\Http\Controllers;
+
+use Nouvu\Web\Foundation\Application AS App;
+use Nouvu\Web\Foundation\User\UserInterface;
+use Nouvu\Web\View\Repository\CommitRepository;
+
+class BackController
+{
+
+}
