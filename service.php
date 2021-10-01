054ef5578a8d42ed9955ed5b54b6d6c36face566
MouseZver <mouse-zver@xaker.ru>
2021-10-01 14:20:01 +0300

    Only Router / Controller / Templates

--- service.php
+++ service.php
@@ -4,6 +4,8 @@ use Nouvu\Web\Foundation\Application;
 
 $load = require 'vendor/autoload.php';
 
+$load -> addPsr4( 'Nouvu\\Web\\', __DIR__ . '/Nouvu' );
+
 return static function ( string $name ) use ( $load ): Application
 {
 	$load -> addPsr4( 'Nouvu\\Resources\\', __DIR__ . "/{$name}/Resources" );
