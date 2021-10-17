8f31b301fd565c0e5d6251fc55351c6a24586bb4
MouseZver <mouse-zver@xaker.ru>
2021-10-17 23:40:05 +0300

    App -> methods[user,granted]

--- src/Nouvu/Foundation/ApplicationTrait.php
+++ src/Nouvu/Foundation/ApplicationTrait.php
@@ -5,6 +5,8 @@ declare ( strict_types = 1 );
 namespace Nouvu\Web\Foundation;
 
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\{ Security, User\UserInterface };
 
 trait ApplicationTrait
 {
@@ -23,8 +25,30 @@ trait ApplicationTrait
 		return $this -> app -> container -> make( $class, $params );
 	}
 	
-	public function path( string $name ): ?string
+	public function path( string $name ): string | null
 	{
 		return $this -> app -> repository -> get( 'app.system.directory.' . $name );
+	}
+	
+	public function getLastUsername(): string | null
+	{
+		return $this -> app -> session -> get( Security :: LAST_USERNAME );
+	}
+	
+	public function getUser(): UserInterface | null
+	{
+		return $this -> app -> container -> get( 'security.token_storage' ) -> getToken() ?-> getUser();
+	}
+
+	public function isGranted( array $attribute, $subject = null ): bool
+	{
+		try
+		{
+			return $this -> app -> security -> isGranted( $attribute, $subject );
+		}
+		catch ( AuthenticationException )
+		{
+			return false;
+		}
 	}
 }
