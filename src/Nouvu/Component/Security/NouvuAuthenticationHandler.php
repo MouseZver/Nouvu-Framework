d21ef6e4df9129cef1c14579e3f6950d2478bba0
MouseZver <mouse-zver@xaker.ru>
2021-10-19 14:57:02 +0300

    Аутентификация пользователя

--- src/Nouvu/Component/Security/NouvuAuthenticationHandler.php
+++ src/Nouvu/Component/Security/NouvuAuthenticationHandler.php
@@ -4,18 +4,9 @@ declare ( strict_types = 1 );
 
 namespace Nouvu\Web\Component\Security;
 
-/* use Symfony\Component\Security\Http\Firewall\ListenerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
-//use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\HttpFoundation\Request; */
-
-
-//use Psr\Container\ContainerInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\{ UserProviderInterface, UserChecker, UserInterface };
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authentication\Token\{ TokenInterface, UsernamePasswordToken };
 use Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider;
 use Nouvu\Web\Component\Security\TokenBasedRememberMeServices;
 use Nouvu\Web\Foundation\Application AS App;
@@ -27,20 +18,40 @@ class NouvuAuthenticationHandler
 		
 	}
 	
-	public function handle( UserInterface $user, string $providerKey, string $secret )
+	public function handle( UserInterface $user, string $providerKey, bool $remember = false )
 	{
 		$UsernamePasswordToken = new UsernamePasswordToken( $user, null, 'secured_area', $user -> getRoles() );
 		
-		$token = $this -> getDaoProvider( new UserChecker, 'secured_area' ) -> authenticate( $UsernamePasswordToken );
+		$token = $this 
+			-> getDaoProvider( new UserChecker, 'secured_area' ) 
+			-> authenticate( $UsernamePasswordToken );
 		
 		$this -> app -> container -> get( 'security.token_storage' ) -> setToken( $token );
 		
-		$this -> getTokenServices( $providerKey, $secret ) -> login( 
-			$this -> app -> request, 
-			$this -> app -> response, 
-			$token 
-		);
+		if ( $remember )
+		{
+			$this -> getTokenServices( $providerKey ) -> login( 
+				$this -> app -> request, 
+				$this -> app -> response, 
+				$token 
+			);
+			
+			$cookies = $this -> app -> response -> headers -> getCookies();
+			
+			$closure = $this -> app -> repository -> get( 'query.database.insert.token' );
+			
+			$closure( $token, $cookies[0] -> getValue() );
+		}
 		
+		$this -> sessionHandle( $user, $token );
+		
+		/* $service = $this -> getTokenServices( $providerKey );
+		
+		$service -> autoLogin( $this -> app -> request ); */
+	}
+	
+	public function sessionHandle( UserInterface $user, TokenInterface $token ): void
+	{
 		$this -> app -> session -> set( $this -> app -> repository -> get( 'security.session_name' ), serialize ( $token ) );
 		
 		$this -> app -> session -> set( Security :: LAST_USERNAME, $user -> getUserIdentifier() );
@@ -56,11 +67,11 @@ class NouvuAuthenticationHandler
 		);
 	}
 	
-	public function getTokenServices( string $providerKey, string $secret )
+	public function getTokenServices( string $providerKey )
 	{
 		return new TokenBasedRememberMeServices( 
 			[ $this -> app -> container -> get( 'security.database.user_provider' ) ], 
-			$secret,
+			$this -> app -> repository -> get( 'security.secret_key' ),
 			$providerKey, 
 			$this -> app -> repository -> get( 'security.remember_me' )
 		);
