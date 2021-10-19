918999b45be4c795249d9ad12762119772124fb1
MouseZver <mouse-zver@xaker.ru>
2021-10-19 15:02:05 +0300

    Доделан и убран мусор

--- userdata/Resources/Controllers/AuthController.php
+++ userdata/Resources/Controllers/AuthController.php
@@ -37,6 +37,8 @@ final class AuthController extends AbstractController
 			
 			$input['_validPassword'] = $input['_userFound'] = true;
 			
+			$input['remember_check'] = isset ( $input['remember_check'] );
+			
 			try
 			{
 				$user = $this -> app -> container -> get( 'security.database.user_provider' ) 
@@ -69,121 +71,14 @@ final class AuthController extends AbstractController
 			{
 				$handler = new \Nouvu\Web\Component\Security\NouvuAuthenticationHandler( $this -> app );
 				
-				$handler -> handle( $user, 'secured_area', 'secret_string' );
+				$handler -> handle( $user, 'secured_area', $input['remember_check'] );
 				
 				//var_dump ( $_SESSION );
 				
-				return $this -> customJson( [ 'success' ] );
+				return $this -> redirect( '/' );
 			}
 		}
 		
 		return $this -> render( 'user/login', 'default-template' );
-	}
-	
-	
-	/* {
-				$daoProvider = new DaoAuthenticationProvider(
-					$this -> app -> container -> get( 'security.database.user_provider' ),
-					new UserChecker(),
-					'secured_area',
-					$this -> app -> container -> get( 'Encoder.factory' ) 
-				);
-				
-				$token = $daoProvider -> authenticate( new UsernamePasswordToken(
-					$user, 
-					null, 
-					'secured_area', 
-					$user -> getRoles()
-				) );
-				
-				// присвоить токен в token_storage
-				$this -> app -> container -> get( 'security.token_storage' ) -> setToken( $token );
-				
-				
-				$rememberMeService = new \Nouvu\Web\Component\Security\TokenBasedRememberMeServices( 
-					[ $this -> app -> container -> get( 'security.database.user_provider' ) ], 
-					'secret_string', 
-					'secured_area', 
-					$this -> app -> repository -> get( 'security.remember_me' )
-				);
-				
-				$rememberMeService -> loginSuccess( $this -> app -> request, $this -> app -> response, $token );
-				
-				$this -> app -> session -> set( '_security_main', serialize ( $token ) );
-				
-				$this -> app -> request -> getSession() -> set( Security :: LAST_USERNAME, $user -> getUserIdentifier() );
-				
-				//$event = new InteractiveLoginEvent( $this -> app -> request, $token );
-				
-				//$this -> app -> container -> get( 'event_dispatcher' ) -> dispatch( 'security.interactive_login', $event );
-	} */
-	
-	
-	public function __login(): CommitRepository
-	{
-		// This data is most likely to be retrieven from the Request object (from Form)
-        // But to make it easy to understand ...
-        $_username = "batman";
-        $_password = "batmobil";
-
-        // Retrieve the security encoder of symfony
-        $factory = $this->get('security.encoder_factory');
-
-        /// Start retrieve user
-        // Let's retrieve the user by its username:
-        // If you are using FOSUserBundle:
-        $user_manager = $this->get('fos_user.user_manager');
-        $user = $user_manager->findUserByUsername($_username);
-        // Or by yourself
-        $user = $this->getDoctrine()->getManager()->getRepository("userBundle:User")
-                ->findOneBy(array('username' => $_username));
-        /// End Retrieve user
-
-        // Check if the user exists !
-        if(!$user){
-            return new Response(
-                'Username doesnt exists',
-                Response::HTTP_UNAUTHORIZED,
-                array('Content-type' => 'application/json')
-            );
-        }
-
-        /// Start verification
-        $encoder = $factory->getEncoder($user);
-        $salt = $user->getSalt();
-
-        if(!$encoder->isPasswordValid($user->getPassword(), $_password, $salt)) {
-            return new Response(
-                'Username or Password not valid.',
-                Response::HTTP_UNAUTHORIZED,
-                array('Content-type' => 'application/json')
-            );
-        } 
-        /// End Verification
-
-        // The password matches ! then proceed to set the user in session
-        
-        //Handle getting or creating the user entity likely with a posted form
-        // The third parameter "main" can change according to the name of your firewall in security.yml
-        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
-        $this->get('security.token_storage')->setToken($token);
-
-        // If the firewall name is not main, then the set value would be instead:
-        // $this->get('session')->set('_security_XXXFIREWALLNAMEXXX', serialize($token));
-        $this->get('session')->set('_security_main', serialize($token));
-        
-        // Fire the login event manually
-        $event = new InteractiveLoginEvent($request, $token);
-        $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
-        
-        /*
-         * Now the user is authenticated !!!! 
-         * Do what you need to do now, like render a view, redirect to route etc.
-         */
-        return new Response(
-            'Welcome '. $user->getUsername(),
-            Response::HTTP_OK,
-            array('Content-type' => 'application/json')
-        );
 	}
 }
