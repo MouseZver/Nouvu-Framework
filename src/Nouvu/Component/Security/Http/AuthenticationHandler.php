efa9cb47b129c73a3ed7d32fd898d97af190401c
MouseZver <mouse-zver@xaker.ru>
2022-06-01 20:24:19 +0300

    New :airplane: Изменение схемы обработки юзера при запросе на сайт

--- src/Nouvu/Component/Security/Http/AuthenticationHandler.php
+++ src/Nouvu/Component/Security/Http/AuthenticationHandler.php
@@ -12,33 +12,43 @@ use Nouvu\Framework\Component\Database\DatabaseManager;
 
 class AuthenticationHandler
 {
+	protected const
+		CONT_MEMORY_USER_PROVIDER = 'security.memory.user_provider',
+		CONT_TOKEN_STORAGE = 'security.token_storage',
+		CONT_REMEMBER_HASHER = 'security.signature_remember_me_hasher';
+		
 	public function __construct ( 
-		private UserProviderInterface $userProvider,
+		/* private UserProviderInterface $userProvider,
 		private RememberMeHandlerInterface $rememberMeHandler,
 		private TokenStorageInterface $storage,
 		private SessionRememberMeHandler $sessionRememberMeHandler,
 		private DatabaseManager $databaseManager,
-		private Request $request,
+		private Request $request, */
 		private string $secret,
 	)
 	{}
 	
 	public function createSession( UserInterface $user, string $providerKey, bool $remember = false )
 	{
-		$this -> sessionRememberMeHandler -> save( $this -> storage, $this -> getToken( $user, $providerKey, $remember ) );
+		$this -> container -> get( \SessionRememberMeHandler :: class ) -> save( 
+			$this -> container -> get( self :: CONT_TOKEN_STORAGE ), 
+			$this -> getToken( $user, $providerKey, $remember ) 
+		);
 	}
 	
 	private function getToken( UserInterface $user, string $providerKey, bool $remember ): TokenInterface
 	{
 		if ( $remember )
 		{
-			$this -> userProvider -> createUser( $user );
+			$this -> container -> get( self :: CONT_MEMORY_USER_PROVIDER ) -> createUser( $user );
 			
-			$this -> rememberMeHandler -> createRememberMeCookie( $user );
+			$this -> container -> get( self :: CONT_REMEMBER_HASHER ) -> createRememberMeCookie( $user );
 			
 			$token = new RememberMeToken( $user, $providerKey, $this -> secret );
 			
-			$this -> databaseManager -> insert( \RememberMeToken :: class )( $token, $this -> sessionRememberMeHandler -> getCookie() -> getValue() );
+			$closure = $this -> container -> get( \Database :: class ) -> insert( \RememberMeToken :: class );
+			
+			$closure( $token, $this -> container -> get( \SessionRememberMeHandler :: class ) -> getCookie() -> getValue() );
 			
 			return $token;
 		}
--- src/Nouvu/Component/Security/Http/SessionRememberMeHandler.php
+++ src/Nouvu/Component/Security/Http/SessionRememberMeHandler.php
@@ -11,18 +11,24 @@ use Symfony\Component\Security\Core\Authentication\Token\{ Storage\TokenStorageI
 use Symfony\Component\Security\Http\RememberMe\{ RememberMeHandlerInterface, ResponseListener };
 use Nouvu\Framework\Component\Database\DatabaseManager;
 use Nouvu\Framework\Component\Security\Core\Provider\DatabaseTokenProvider;
-//use Nouvu\Framework\Http\Kernel;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 
 class SessionRememberMeHandler
 {
+	protected const
+		CONT_DATABASE_TOKEN_PROVIDER = 'security.database.token_provider',
+		CONT_DATABASE_USER_PROVIDER = 'security.database.user_provider',
+		CONT_REMEMBER_HASHER = 'security.signature_remember_me_hasher';
+	
 	public function __construct ( 
-		//private Kernel $kernel,
-		private DatabaseTokenProvider $tokenProvider,
+		/* private DatabaseTokenProvider $tokenProvider,
 		private RememberMeHandlerInterface $rememberMeHandler,
 		private DatabaseManager $databaseManager,
 		private Request $request,
 		private Response $response,
-		private SessionInterface $session,
+		private SessionInterface $session, */
+		private ContainerInterface $container,
 		private string $cookie_name, 
 		private string $session_name,
 	)
@@ -30,60 +36,83 @@ class SessionRememberMeHandler
 	
 	public function push( TokenStorageInterface $storage ): void
 	{
-		$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		//$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		$series = $this -> container -> get( \Request :: class ) -> cookies -> get( $this -> cookie_name );
 		
-		$session_token = $this -> session -> get( $this -> session_name );
+		//$session_token = $this -> session -> get( $this -> session_name );
+		$session_token = $this -> container -> get( \Session :: class ) -> get( $this -> session_name );
 		
 		if ( ! is_null ( $series ) && is_null ( $session_token ) )
 		{
 			try
 			{
-				$this -> login( $storage, $series );
+				//$this -> login( $storage, $series );
+				$this -> container -> get( \AuthenticationHandler :: class ) -> login( $storage, $series );
 			}
 			catch ( AuthenticationException )
 			{
-				$this -> response -> headers -> clearCookie( $this -> cookie_name );
+				//$this -> response -> headers -> clearCookie( $this -> cookie_name );
+				$this -> clear( $storage );
 			}
 		}
 		else if ( ! is_null ( $session_token ) )
 		{
-			$this -> save( $storage, unserialize ( $session_token ) );
+			$email = unserialize ( $session_token ) -> getUser() -> getEmail();
+			
+			try
+			{
+				$user = $this -> container -> get( self :: CONT_DATABASE_USER_PROVIDER ) -> loadUserByIdentifier( $email );
+				
+				$this -> container -> get( \AuthenticationHandler :: class ) -> createSession( $user, 'secured_area' );
+			}
+			catch ( UserNotFoundException )
+			{
+				$this -> clear( $storage );
+			}
 		}
 	}
 	
 	public function login( TokenStorageInterface $storage, string $series ): void
 	{
-		$token = $this -> tokenProvider -> loadTokenByIdentifier( $series );
+		//$token = $this -> tokenProvider -> loadTokenByIdentifier( $series );
+		$token = $this -> container -> get( self :: CONT_DATABASE_TOKEN_PROVIDER ) -> loadTokenByIdentifier( $series );
 		
-		$this -> rememberMeHandler -> createRememberMeCookie( $token -> getUser() );
+		//$this -> rememberMeHandler -> createRememberMeCookie( $token -> getUser() );
+		$this -> container -> get( self :: CONT_REMEMBER_HASHER ) -> createRememberMeCookie( $token -> getUser() );
 		
 		$this -> save( $storage, $token );
 	}
 	
 	public function getCookie(): ?Cookie
 	{
-		return $this -> request -> attributes -> get( ResponseListener :: COOKIE_ATTR_NAME );
+		//return $this -> request -> attributes -> get( ResponseListener :: COOKIE_ATTR_NAME );
+		return $this -> container -> get( \Request :: class ) -> attributes -> get( ResponseListener :: COOKIE_ATTR_NAME );
 	}
 	
 	public function save( TokenStorageInterface $storage, TokenInterface $token ): void
 	{
 		$storage -> setToken( $token );
 		
-		$this -> session -> set( $this -> session_name, serialize ( $token ) );
+		//$this -> session -> set( $this -> session_name, serialize ( $token ) );
+		$this -> container -> get( \Session :: class ) -> set( $this -> session_name, serialize ( $token ) );
 		
-		$this -> session -> set( Security :: LAST_USERNAME, $token -> getUser() -> getUsername() );
+		//$this -> session -> set( Security :: LAST_USERNAME, $token -> getUser() -> getUsername() );
+		$this -> container -> get( \Session :: class ) -> set( Security :: LAST_USERNAME, $token -> getUser() -> getUsername() );
 		
 		$cookie = $this -> getCookie();
 		
-		$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		//$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		$series = $this -> container -> get( \Request :: class ) -> cookies -> get( $this -> cookie_name );
 		
 		if ( ! empty ( $cookie ) )
 		{
-            $this -> response -> headers -> setCookie( $cookie );
+            //$this -> response -> headers -> setCookie( $cookie );
+			$this -> container -> get( \Response :: class ) -> headers -> setCookie( $cookie );
 			
 			if ( ! empty ( $series ) )
 			{
-				$closure = $this -> databaseManager -> update( \RememberMeToken\Series\By\Series :: class );
+				//$closure = $this -> databaseManager -> update( \RememberMeToken\Series\By\Series :: class );
+				$closure = $this -> container -> get( \Database :: class ) -> update( \RememberMeToken\Series\By\Series :: class );
 				
 				$closure( $cookie -> getValue(), $series );
 			}
@@ -92,17 +121,20 @@ class SessionRememberMeHandler
 	
 	public function clear( TokenStorageInterface $storage ): void
 	{
-		$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		//$series = $this -> request -> cookies -> get( $this -> cookie_name );
+		$series = $this -> container -> get( \Request :: class ) -> cookies -> get( $this -> cookie_name );
 		
 		if ( ! empty ( $series ) )
 		{
-			$this -> databaseManager -> delete( \RememberMeToken\By\Series :: class )( $series );
+			//$this -> databaseManager -> delete( \RememberMeToken\By\Series :: class )( $series );
+			$this -> container -> get( \Database :: class ) -> delete( \RememberMeToken\By\Series :: class )( $series );
 			
-			$this -> response -> headers -> clearCookie( $this -> cookie_name );
+			//$this -> response -> headers -> clearCookie( $this -> cookie_name );
+			$this -> container -> get( \Response :: class ) -> headers -> clearCookie( $this -> cookie_name );
 		}
 		
 		$storage -> setToken( null );
 		
-		$this -> request -> getSession() -> invalidate();
+		$this -> container -> get( \Request :: class ) -> getSession() -> invalidate();
 	}
 }
